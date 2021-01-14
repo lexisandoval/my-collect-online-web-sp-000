91333d620b614c8d25d9218ba65fe3287dcfc953
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 21:01:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(arr)
-  i = 0
- arrn = []
- while i < arr.length
-   arrn << yield(arr[i])
-   i = i + 1
- end
+   i = 0
+   arrn = []
+   while i < arr.length
+     arrn << yield(arr[i])
+     i = i + 1
+   end
 
- arrn
+   arrn
 end
